886bc77b5a38a53c062a94f3fe8021e494acb8df
Mac <mac@Ruslans-MacBook-Pro.local>
2024-08-16 08:52:55 +0200

    first commit

---
+++ utils/birthdayWishes.js
@@ -0,0 +1,34 @@
+const messages = [
+    { "value": "May you be blessed with life’s greatest joys and endless happiness. You are a gift to the world, and you deserve the very best!" },
+    { "value": "Don’t count the candles, but see the light they give. Don’t count the years, but the life you live. Wishing you a wonderful journey ahead!" },
+    { "value": "Forget the past; embrace the future, for the best is yet to come!" },
+    { "value": "You are only young once, but you can stay youthful forever!" },
+    { "value": "Cheers to you for another year of life and love!" },
+    { "value": "Wishing you a day filled with good health, happiness, and all the things you love most." },
+    { "value": "As Shakespeare might say: ‘Party like there’s no tomorrow!’" },
+    { "value": "Don’t forget to smile awkwardly as everyone sings ‘Happy Birthday’ to you today!" },
+    { "value": "Birthdays are nature’s way of telling us to eat more cake and celebrate!" },
+    { "value": "On your birthday, I’ll share the secret to staying young: always lie about your age." },
+    { "value": "Work hard, play hard, and eat lots of cake. That’s the perfect motto for your birthday and for life." },
+    { "value": "May your birthday be filled with gifts, love, and lots of fun!" },
+    { "value": "May your heart be filled with love, your day with happiness, and your life with blessings." },
+    { "value": "Be happy! Today is the day you were brought into this world to be a blessing and inspiration to those around you. You are amazing! May you have many more birthdays to fulfill all your dreams!" },
+    { "value": "Forget about the past, you can’t change it. Forget about the future, you can’t predict it. And forget about the present, because I didn’t get you one. Happy birthday!" },
+    { "value": "As you get older, three things happen. The first is your memory goes, and I can’t remember the other two. Happy birthday!" },
+    { "value": "When the little kids ask how old you are at your party, tell them. While they’re distracted trying to count that high, you can steal a bite of their cake! Happy birthday!" },
+    { "value": "Sending you smiles for every moment of your special day. Have a wonderful time and a very happy birthday!" },
+    { "value": "Sending you a birthday wish wrapped with all my love. Have a fantastic birthday!" },
+    { "value": "A simple celebration, a gathering of friends; wishing you great happiness and a joy that never ends. Happy birthday!" },
+    { "value": "Hope your birthday is as amazing as you are!" },
+    { "value": "I hope that today, at your party, you dance and others sing as you celebrate with joy your best birthday yet." },
+    { "value": "It’s always a treat to wish a happy birthday to someone so sweet." },
+    { "value": "Hey you! It’s your birthday! Cake! Candles! Drinks! Presents! More food! Can you tell I’m excited?" },
+    { "value": "Happy birthday to my forever young friend!" },
+    { "value": "Birthdays only come once a year, but your friendship is once in a lifetime." },
+    { "value": "You deserve all the cake, happiness, and love today. Happy birthday!" },
+    { "value": "Thank you for being the brightest person I know. I hope to see you shine for many more years! Happy birthday!" },
+];
+
+
+export default messages;
+
